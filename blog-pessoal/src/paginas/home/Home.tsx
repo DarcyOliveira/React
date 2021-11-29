40f333cd0937db66758b8c73560424df971692c2
DarcyOliveira <darcyelleoliveira@gmail.com>
2021-11-29 00:24:36 -0300

    Criando e estilizando primeiro componente com CSS

---
+++ blog-pessoal/src/paginas/home/Home.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Home.css";
+
+function Home() {
+    return (
+        <>
+        <h1 className = "titulo">Home</h1>
+        <img src="https://i.imgur.com/bucmOCc.jpg" alt="Imagem de tela inicial" className="img" />
+        </>
+    );
+}
+
+export default Home;
